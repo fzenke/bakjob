4a47b802d5649fce9ca108713a55e535b0738170
Friedemann Zenke <fzenke@stanford.edu>
2016-04-29 15:26:00 +0200

    Adds config file support and CLI interface

--- src/bakjob.py
+++ src/bakjob.py
@@ -1,44 +1,31 @@
 #!/usr/bin/python
 
+import sys
 import socket
 import time
 import logging
+import argparse
+import md5
+import ConfigParser
+import pickle
 
 from urlparse import urlparse
 from subprocess import call
 
-import argparse
 
 
+# Parse command line arguments
 parser = argparse.ArgumentParser(description='Runs jobs regularly when certain cloud services or paths become available.')
 parser.add_argument('--configfile', type=str, default="bakjob.conf", help='Config file')
 parser.add_argument('--logfile', type=str, default="bakjob.log", help='Log file')
+parser.add_argument('--statefile', type=str, default="bakjob.dat", help='Save file for last run times')
 parser.add_argument('--quiet', action='store_true', help='Do not log to console')
 parser.add_argument('--verbose', '-v', action='count', help='Verbosity level')
 parser.add_argument('--sleeptime', type=int, default=600, help='Time to sleep in seconds between checks if there is work.')
 args = parser.parse_args()
-
 # parameters
 sleep_time = args.sleeptime
 
-
-def check_host_availability(hostname, port=22):
-    if port == None:
-        port = 22
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        s.connect((hostname, port))
-        logger.info("Port %i on %s reachable"%(port, hostname))
-        return_value = True
-    except socket.error as e:
-        return_value = False
-        logger.info("Error on connect: %s" % e)
-    s.close()
-    return return_value
-
-def check_path_availability(path):
-    # TODO implement 
-    return True
 
 # Set up logging
 logger = logging.getLogger('bakjob')
@@ -63,34 +50,82 @@ if args.logfile:
     logger.addHandler(fh)
 
 
+# Load run data
+#
+try:
+    pkl_file = open(args.statefile, 'rb')
+    rundata = pickle.load(pkl_file)
+    pkl_file.close()
+except IOError:
+    rundata = {}
+    logger.warning("Could not open statefile %s"%args.statefile)
+
+# parse config file
+bakjobs = [ ]
+config = ConfigParser.RawConfigParser()
+config.read(args.configfile)
+for section in config.sections():
+    m = md5.new(config.get(section,'cmd').__str__())
+    md5hash = m.digest()
+    last_run_time = 0
+    if md5hash in rundata.keys():
+        last_run_time = rundata[md5hash]
+        logger.debug("Loading runtime for %s"%section)
+    else:
+        logger.debug("No runtime for %s saved, scheduled for running"%section)
+    job = { 'name'     : section, 
+            'target'   : config.get(section,'target') , 
+            'cmd'      : config.get(section,'cmd') , 
+            'interval' : config.getint(section,'interval') ,
+            'md5hash'  : md5hash ,
+            'last_run_time' : last_run_time
+            }
+    bakjobs.append(job)
+
+def check_host_availability(hostname, port=22):
+    if port == None:
+        port = 22
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((hostname, port))
+        logger.info("Port %i on %s reachable"%(port, hostname))
+        return_value = True
+    except socket.error as e:
+        return_value = False
+        logger.info("Error on connect: %s" % e)
+    s.close()
+    return return_value
+
+def check_path_availability(path):
+    # TODO implement 
+    return True
+
+def save_last_run_times(jobs):
+    data = {}
+    for job in jobs:
+        data[job['md5hash']] = job['last_run_time']
+
+    try:
+        pkl_file = open(args.statefile, 'w')
+        pickle.dump(data, pkl_file)
+        pkl_file.close()
+    except IOError:
+        rundata = {}
+        logger.warning("Could not open statefile %s for writing"%args.statefile)
 
 
 
-
-# 1st test job
-bak_target   = 'www://www.fzenke.net'
-bak_cmd      = 'ping -c 3 fzenke.net'
-bak_interval = 3600 # a much larger interval in seconds
-# bak_path = 'file:///home/zenke/bak'
-job = { 'target' : bak_target, 'cmd' : bak_cmd, 'interval' : bak_interval }
-bakjobs = [ job ]
-
-# 2nd test job
-bak_target   = 'rsync://fzenke.net/bak/arthur'
-bak_cmd      = '/home/zenke/bin/backup_personal.sh'
-bak_interval = 3600*12 # a much larger interval in seconds
-job = { 'target' : bak_target, 'cmd' : bak_cmd, 'interval' : bak_interval }
-
-bakjobs.append(job)
-
-
-logger.info("bakjob started with %i jobs in conduit"%len(bakjobs))
+if len(bakjobs):
+    logger.info("bakjob started with %i jobs in conduit"%len(bakjobs))
+else:
+    logger.error("No jobs in config file! Exiting.")
+    sys.exit(1)
 
 while True:
     for jobid,job in enumerate(bakjobs):
         time.sleep(sleep_time)
 
-        logger.debug("Checking task %i"%jobid)
+        logger.debug("Checking job %i (%s)"%(jobid, job['name']))
         if 'last_run_time' in job.keys():
             next_run_time = job['last_run_time']+job['interval']
             if time.time()<next_run_time:
@@ -99,11 +134,9 @@ while True:
 
         url = urlparse(job['target'])
         if url.scheme == 'file':
+            # TODO Implement logic for paths
             logger.warning("Do something locally")
         else:
-            # print url.scheme
-            # print url.hostname
-            # print url.port
             if check_host_availability(url.hostname, url.port):
                 logger.info("Host %s is available, running job %s: %s"%(url.hostname, jobid, job['cmd']))
                 errorcode = call(job['cmd'], shell=True)
@@ -112,5 +145,6 @@ while True:
                 else:
                     logger.info("Job %i finished sucessfully. Next run in >%is from now."%(jobid,job['interval']))
                     job['last_run_time'] = time.time()
+                    save_last_run_times(bakjobs)
 
 
